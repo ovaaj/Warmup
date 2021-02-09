a27aa1ee969dddbcc60222ce5d795111e3efef1f
ovaaj <sleepy-t@comcast.net>
2021-02-08 19:29:28 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("Setting my booby trap!");
+$("button").click(function() {
+    console.log("Attempting to hide the panda!");
+    $("img").toggle();
+
+});
+
+console.log("the panda strikes back!");
